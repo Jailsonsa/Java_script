3b0fc6d350e52798442c4f97d020a48222027478
git/ <jailsonsilvacom1@gmail.com>
2022-01-27 16:43:00 -0300

    Colocando aula sobre SWITCH

---
+++ 008 - Switch/switch.js
@@ -0,0 +1,20 @@
+//SWITCH
+
+let valor = 3;
+    switch(valor){
+        case 1:
+            console.log("O numero é 1");
+            break;
+
+        case 2:
+            console.log("O numero é 2");
+            break;
+
+        case 3:
+            console.log("O numero é 3");
+            break;
+            
+        default:
+            console.log("Num não encontrado");
+            break;
+    }
