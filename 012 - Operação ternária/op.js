4303b40af82e1f19ece64e175f5c1c405d1a6bf5
git/ <jailsonsilvacom1@gmail.com>
2022-02-07 15:10:57 -0300

    Operção ternaria

---
+++ 012 - Operação ternária/op.js
@@ -0,0 +1,4 @@
+// (Codição) ? 'Valor para verdadeiro' : 'Valor para falso';
+ const pontuacaoDoAluno = 100;
+ const pontuacaoDesejada = pontuacaoDoAluno >= 1000 ? "Nivel VIP" : "Nivel NORMAL";
+ console.log(pontuacaoDesejada);
