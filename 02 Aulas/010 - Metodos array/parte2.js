793409dc204b5e41b6a8782e4130dba468c6f9f9
git/ <jailsonsilvacom1@gmail.com>
2022-04-12 14:23:15 -0300

    Métodos array parte final

---
+++ 02 Aulas/010 - Metodos array/parte2.js
@@ -0,0 +1,14 @@
+console.clear();
+// splice 
+
+var arr = [1, 2, 3, 4, 5];
+arr.splice(4, 0, 4.5);
+console.log(arr);
+
+arr.splice(3, 1);
+console.log(arr);
+
+// indexOf
+
+console.log(arr.indexOf(0));
+
