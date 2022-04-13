59e1d1acaa820a8d145374f9b2c857a611cc3051
git/ <jailsonsilvacom1@gmail.com>
2022-04-13 12:38:44 -0300

    Vendo como acessar DOM novamente

---
+++ 02 Aulas/012 - Acessando DOM/dom.js
@@ -0,0 +1,20 @@
+// ACESSANDO O DOM 
+
+// Tag
+/*
+
+let titulo = document.getElementsByTagName("h1")[0];
+console.log(titulo);
+
+let lis = document.getElementsByTagName('li')
+console.log(lis);
+
+// Id 
+let paragrafo = document.getElementById("paragrafo");
+console.log(paragrafo);
+
+// class
+let itensDaLista = document.getElementsByClassName('item');
+console.log(itensDaLista);
+
+*/
---
+++ 02 Aulas/012 - Acessando DOM/query.js
@@ -0,0 +1,13 @@
+// QuerySelectorAll()
+let itensQuery = document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+let lista_1 = document.querySelectorAll('#lista1 .item');
+console.log(lista_1);
+
+// QuerySelector()
+let lista = document.querySelector('#lista1');
+console.log(lista);
+
+let spanT = document.querySelector('#paragrafo span');
+console.log(spanT);
